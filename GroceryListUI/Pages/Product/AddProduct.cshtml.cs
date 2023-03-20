7e1de970372a1a2f90db005745463533376bad7b
buckboat <123005977+buckboat@users.noreply.github.com>
2023-03-20 15:07:27 -0500

    Update in calss

--- GroceryListUI/Pages/Product/AddProduct.cshtml.cs
+++ GroceryListUI/Pages/Product/AddProduct.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using GroceryListUI.Pages.Models;
 
 namespace GroceryListUI.Pages.Product
 {
@@ -10,6 +12,15 @@ namespace GroceryListUI.Pages.Product
         public GroceryListUI.Pages.Models.Product NewProduct { get; set; } = new GroceryListUI.Pages.Models.Product();
         public void OnGet()
         {
+            /*
+             * 1. Create a SQL connection object 
+             * 2. Construct a SQL statement
+             * 3. Create a SQL command object
+             * 4. Open the SQL connection
+             * 5. Execute the SQL command
+             * 6. Close the SQL connection
+             */
+
             NewProduct.ProductName = "Name";
             NewProduct.ImageURL = "URL";
             NewProduct.NutritoinLabel = "Nutrition";
@@ -23,6 +34,22 @@ namespace GroceryListUI.Pages.Product
 
         public void OnPost()
         {
+            // step 1
+            SqlConnection conn = new SqlConnection("Server=(LocalDB)\\MSSQLLocalDB;Database=Grocery;Trusted_Connection=true;");
+            // step 2
+            string sql = "INSERT INTO Product(ProductName,ImageURL,NutritionLabel,Description,Price,Ingredients,Quantity)" +
+                "VALUES('" + NewProduct.ProductName +"','"+ NewProduct.ImageURL+"','" + NewProduct.Price + "','" + NewProduct.Description +
+                "','" + NewProduct.Price + "','" + NewProduct.Ingredients + "','" + NewProduct.Quantity + "')";
+            //step 3
+            SqlCommand cmd = new SqlCommand(sql,conn);
+            //step 4
+            conn.Open();
+            //step 5
+            cmd.ExecuteNonQuery();
+            //step 6
+            conn.Close();
+ 
+
 
         }
     }
