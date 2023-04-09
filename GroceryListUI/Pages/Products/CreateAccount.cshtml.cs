0d04451dcb22361522862de4e184e7266280a2b6
wilsonjj3 <122841036+wilsonjj3@users.noreply.github.com>
2023-04-09 16:46:46 -0500

    Added some pages and linked checkout

---
+++ GroceryListUI/Pages/Products/CreateAccount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GroceryListUI.Pages.Products
+{
+    public class CreateAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
