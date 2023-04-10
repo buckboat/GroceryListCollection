a223eb638bdda396228e9611277ab0d7adbcfca2
buckboat <123005977+buckboat@users.noreply.github.com>
2023-04-10 12:11:05 -0500

    add user page

---
+++ GroceryListUI/Pages/Account/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GroceryListUI.Pages.Account
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
