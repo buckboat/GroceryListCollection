33ded59bf70f5b70bde46e0471919bfbfdc01623
wilsonjj3 <122841036+wilsonjj3@users.noreply.github.com>
2023-04-09 19:21:11 -0500

    Checkout added and put a total page at the bottom

---
+++ GroceryListUI/Pages/Products/TotalPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GroceryListUI.Pages.Products
+{
+    public class TotalPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
