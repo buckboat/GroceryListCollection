82a41703a9b88ae9b860c97447f4237c3745fd86
buckboat <123005977+buckboat@users.noreply.github.com>
2023-04-10 00:55:43 -0500

    tried to add working serach

--- GroceryListUI/Pages/Index.cshtml.cs
+++ GroceryListUI/Pages/Index.cshtml.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
 using GroceryListUI.Pages.Models;
 
+
+
+
 namespace GroceryListUI.Pages
 {
+    
+    
+
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
@@ -22,17 +28,53 @@ namespace GroceryListUI.Pages
              * 5. Execute the SQL command
              * 6. Close the SQL connection
         */
-
+        [BindProperty]
         public GroceryListUI.Pages.Models.ListProduct NewListProduct { get; set; } = new GroceryListUI.Pages.Models.ListProduct();
-
-        public void OnGet()
+        
+        public IActionResult OnGet(string search)
         {
+            // step 1
+            using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
+            {
+                // step 2
+                string sql = "SELECT * FROM Product WHERE ProductName ='%" + search + "%'Order by ProductName";
+                //step 3
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //step 4
+                conn.Open();
+                //step 5
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+
+                        Product product = new Product();
+                        product.ProductID = int.Parse(reader["ProductID"].ToString());
+                        product.ProductName = reader["ProductName"].ToString();
+                        product.ImageURL = reader["ImageURL"].ToString();
+                        product.NutrtionLabel = reader["NutrtionLabel"].ToString();
+                        product.Description = reader["Description"].ToString();
+                        product.Price = decimal.Parse(reader["Price"].ToString());
+                        product.Ingredient = reader["Ingredient"].ToString();
+                        product.Quantity = int.Parse(reader["Quantity"].ToString());
+
+                    }
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return Page();
+                }
+            }
+
         }
-        public IActionResult OnPost(int id)
+        public IActionResult OnPost(string search)
         {
             if (ModelState.IsValid)
             {
                 // step 1
+                /*
                 using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
                 {
 
@@ -48,6 +90,39 @@ namespace GroceryListUI.Pages
                     //step 5
                     cmd.ExecuteNonQuery();
                 }
+                */
+                using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
+                {
+                    // step 2
+                    string sql = "SELECT * FROM Product WHERE ProductName ='%" + search + "%'Order by ProductName";
+                    //step 3
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    //step 4
+                    conn.Open();
+                    //step 5
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+
+                            Product product = new Product();
+                            product.ProductID = int.Parse(reader["ProductID"].ToString());
+                            product.ProductName = reader["ProductName"].ToString();
+                            product.ImageURL = reader["ImageURL"].ToString();
+                            product.NutrtionLabel = reader["NutrtionLabel"].ToString();
+                            product.Description = reader["Description"].ToString();
+                            product.Price = decimal.Parse(reader["Price"].ToString());
+                            product.Ingredient = reader["Ingredient"].ToString();
+                            product.Quantity = int.Parse(reader["Quantity"].ToString());
+
+                        }
+                    }
+
+                }
+
+
+
                 return RedirectToAction("Index");
             }
             else
