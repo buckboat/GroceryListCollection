87067aac3cc075406abdd9f9238ffbe4d0fb449c
buckboat <123005977+buckboat@users.noreply.github.com>
2023-04-19 14:47:03 -0500

    adding account info to database

--- GroceryListUI/Pages/Account/Index.cshtml.cs
+++ GroceryListUI/Pages/Account/Index.cshtml.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using GroceryListUI.Pages.Models;
 
 namespace GroceryListUI.Pages.Account
 {
     public class IndexModel : PageModel
     {
         //[BindProperty]
-        public UserCreds User { get; set; } = new UserCreds();
+        public User NewUser { get; set; } = new User();
+        //public UserCreds User { get; set; } = new UserCreds();
 
         public class UserCreds
         {
@@ -19,28 +22,44 @@ namespace GroceryListUI.Pages.Account
             public string Password { get; set; }
 
 
-
-
         }
 
         public void OnGet()
         {
         }
 
-        public void OnPost()
+        public IActionResult OnPost()
         {
 
-            if (ModelState.IsValid)
+                if (ModelState.IsValid)
             {
-
-                if (User.Email == "admin@mysite.com" && User.Password == "Password")
+                // step 1
+                using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
                 {
-                    // creds verified
 
-                    //create secuirty context
+                    // step 2
+                    string sql = "INSERT INTO User(FirstName,LastName,Email,Password)" +
+                        "VALUES(@firstName, @lastName, @email, @password)";
+                    //step 3
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@firstName", NewUser.FirstName);
+                    cmd.Parameters.AddWithValue("@lastName", NewUser.LastName);
+                    cmd.Parameters.AddWithValue("@email", NewUser.Email);
+                    cmd.Parameters.AddWithValue("@password", NewUser.Password);
+                   // cmd.Parameters.AddWithValue("@price", NewProduct.Price);
+                   // cmd.Parameters.AddWithValue("@ingredient", NewProduct.Ingredient);
+                   // cmd.Parameters.AddWithValue("@quantity", NewProduct.Quantity);
+
+                    //step 4
+                    conn.Open();
+                    //step 5
+                    cmd.ExecuteNonQuery();
                 }
-
-
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return Page();
             }
 
         }
---
+++ GroceryListUI/Pages/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroceryListUI.Pages.Models
+{
+
+    public class User
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //[Required]
+        public string Email { get; set; }
+        //[Required]
+        public string Password { get; set; }
+
+
+
+
+    }
+
+}
