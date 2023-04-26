0461fc0259a68b61d958b027c4e33db0766ebefa
buckboat <123005977+buckboat@users.noreply.github.com>
2023-04-26 15:58:54 -0500

    update checkout

--- GroceryListUI/Pages/Products/Checkout.cshtml.cs
+++ GroceryListUI/Pages/Products/Checkout.cshtml.cs
@@ -2,15 +2,72 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using GroceryListUI.Pages.Models;
+using static GroceryListUI.Pages.IndexModel;
+using Microsoft.Data.SqlClient;
+
 
 
 namespace GroceryListUI.Pages.Products
 {
+
+
+    
+
     [Authorize]
     public class CheckoutModel : PageModel
     {
-        public void OnGet()
+
+        [BindProperty]
+        public List<UserList> Bob { get; set; } = new List<UserList>();
+
+   
+
+       
+        public IActionResult OnGet()
         {
+
+
+                using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
+                {
+                    // step 2
+                    string sql = "SELECT Product.ProductID, Product.ImageURL, Product.ProductName, Product.Price,List.ListID,ListProduct.ProductQuantity,ListProduct.Discount " +
+                       "FROM ((Product INNER JOIN ListProduct ON Product.ProductID = ListProduct.ProductID)" +
+                                      "INNER JOIN List ON List.ListID = ListProduct.ListID) WHERE List.ListId = 1 Order by Product.ProductName";
+
+                    //step 3
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    //step 4
+                    conn.Open();
+                    //step 5
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+
+                            UserList uList = new UserList();
+
+
+
+                            //uList.ListID = int.Parse(reader["1"].ToString());
+                            uList.ProductID = int.Parse(reader["ProductID"].ToString());
+                            uList.ImageURL = reader["ImageURL"].ToString();
+                            uList.Price = decimal.Parse(reader["Price"].ToString());
+                            uList.ProductName = reader["ProductName"].ToString();
+                            uList.Quantity = int.Parse(reader["ProductQuantity"].ToString());
+                            Bob.Add(uList);
+
+                        }
+                        return Page();
+                    }
+                    else
+                    {
+                        return Page();
+                    }
+                }
+            
+
         }
+        
     }
 }
--- ShoppingListDB/dbo/Tables/LIstProduct.sql
+++ ShoppingListDB/dbo/Tables/LIstProduct.sql
@@ -1,10 +1,15 @@
 ﻿CREATE TABLE [dbo].[LIstProduct] (
-    [ListID]          INT NOT NULL,
-    [ProductID]       INT NULL,
-    [ProductQuantity] INT NULL,
+    [ListID]          INT          NOT NULL,
+    [ProductID]       INT          NULL,
+    [ProductQuantity] INT          CONSTRAINT [DF_LIstProduct_ProductQuantity] DEFAULT ((1)) NULL,
+    [Discount]        DECIMAL (18) CONSTRAINT [DF_LIstProduct_Discount] DEFAULT ((0)) NULL,
     CONSTRAINT [FK_LIst_Product] FOREIGN KEY ([ListID]) REFERENCES [dbo].[List] ([ListID]),
     CONSTRAINT [FK_Product_LIst] FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Product] ([ProductID])
 );
+
+
+
+
 
 
 
